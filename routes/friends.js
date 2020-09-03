e0b6342dace0165e7a15c60c898f1f1e193a4f32
TribbianniSun <49214460+TribbianniSun@users.noreply.github.com>
2020-09-03 23:30:15 +0800

    more

--- routes/friends.js
+++ routes/friends.js
@@ -2,13 +2,37 @@ const express = require('express')
 const router = express.Router()
 
 // All Friends Route
-router.get('/', (req, res) => {
-  res.render('friends/index')
+router.get('/', async (req, res) => {
+  try{
+    const currentRequests = await Friend.find({ recipient: req.user.id}).lean()
+    res.render('friend/index', {
+      requester: currentRequests.requester,
+    })
+  }
+  catch (err) {
+    console.error(err)
+  }
 })
 
 // Add New Friend Route
-router.get('/add', (req, res) => {
-  res.render('friends/add')
+
+
+router.post('/', ensureAuth, async (req, res) => {
+  try {
+    req.body.requester = req.user.id
+    req.body.recipient = req.user.id // still trying to get the id of the recipient
+    await Friend.create(req.body)
+    res.redirect('/home')
+  } catch (err) {
+    console.error(err)
+  }
 })
+
+
+
+
+
+
+
 
 module.exports = router
